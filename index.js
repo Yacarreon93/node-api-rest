1d220722c99898c48a9caad6872edf23e594d8fc
Yasser Carreón <yacasia@gmail.com>
2016-11-20 17:05:44 -0600

    Included body parser on "index.js"

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-	console.log('API REST runs on http://localhost:3000')
+app.listen(port, () => {
+
+	console.log(`API REST runs on http://localhost:${port}`)
 
 })
