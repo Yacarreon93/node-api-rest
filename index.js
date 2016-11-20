242b2b426a13feb45db2eab84a63596977bb549a
Yasser Carreón <yacasia@gmail.com>
2016-11-20 17:19:03 -0600

    Included http get request with params

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/hello', (req, res) => {
+
+	res.send({ message: 'Hello World!' });
+
+})
+
+app.get('/hello/:name', (req, res) => {
+
+	res.send({ message: `Hello ${req.params.name}` });
+
+})
+
+
 app.listen(port, () => {
 
 	console.log(`API REST runs on http://localhost:${port}`)
