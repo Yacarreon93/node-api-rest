5685b5afe8e5b431ff95e1a6620e0e95a8415f02
Yasser Carreón <yacasia@gmail.com>
2016-11-20 18:31:57 -0600

    Added product API routes

--- index.js
+++ index.js
@@ -9,18 +9,43 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => {
+// app.get('/hello', (req, res) => {
+// 	res.send({ message: 'Hello World!' });
+// })
 
-	res.send({ message: 'Hello World!' });
+// app.get('/hello/:name', (req, res) => {
+// 	res.send({ message: `Hello ${req.params.name}` });
+// })
+
+app.get('/api/products', (req, res) => {
+
+	res.status(200).send({ products: [] })
 
 })
 
-app.get('/hello/:name', (req, res) => {
+app.get('/api/product/:productId', (req, res) => {
 
-	res.send({ message: `Hello ${req.params.name}` });
+	
+})
+
+app.post('/api/product', (req, res) => {
+
+	console.log(req.body)
+	res.status(200).send({ message: "The product was received"})
 
 })
 
+app.put('/api/product/:productId', (req, res) => {
+
+
+	
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+
+	
+})
 
 app.listen(port, () => {
 
